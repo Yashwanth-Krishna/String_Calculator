b3ec2170039191d151c40dcf3897da9656c6155b
Yashwanth-Krishna <95580363+Yashwanth-Krishna@users.noreply.github.com>
2024-10-16 18:08:33 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+function addNumbers(input) {
+    let finalSum;
+
+    if (input.length > 0) {
+        let numArr  = [];
+        let negativeNumbers = [];
+
+        let checkPositiveOrNegative = input.match(/-?\d+/g);
+
+        negativeCount = 0;
+
+        for (let each of checkPositiveOrNegative){
+            if (Number(each) > 0) {
+                numArr.push(Number(each));
+            } else {
+                negativeNumbers.push(Number(each))
+                negativeCount++;
+            }
+        }
+
+    if (negativeCount > 0) {
+        finalSum  = `Cant add these negative numbers ${negativeNumbers}`;
+    } else {
+       finalSum =  numArr.reduce((acc,num) => acc+num, 0)
+    }
+    } else {
+         finalSum = 0
+    }
+    
+    return finalSum
+}
+
+const result = addNumbers("//;\n1;-2-3-4");
+console.log(result)
